372e10a18028bde86265151d12721d8d0ed28cc3
xieby1 <xieby1@outlook.com>
2025-01-12 21:14:48 +0800

    symlink join ymls

--- actions.nix
+++ actions.nix
@@ -1,22 +1,27 @@
 let
   pkgs = import <nixpkgs> {};
   mypkgs = import ./mypkgs.nix;
-in builtins.mapAttrs (name: value:
-  (pkgs.formats.yaml {}).generate "${name}.yml" {
-    inherit name;
-    on = {
-      push = {
-        branches = ["main"];
-    };};
-
-    jobs = {
-      "${name}" = {
-        runs-on = "ubuntu-latest";
-        steps = [
-          { uses = "actions/checkout@v4"; }
-          { uses = "cachix/install-nix-action@v23";
-            "with" = {nix_path = "nixpkgs=channel:nixos-24.05";}; }
-          { run = "nix-build ./mypkgs.nix -A ${name}"; }
-    ];};};
-  }
-) mypkgs
+  ymls = pkgs.lib.mapAttrsToList (name: value:
+    (pkgs.formats.yaml {}).generate "${name}.yml" {
+      inherit name;
+      on = {
+        push = {
+          branches = ["actions"];
+      };};
+      jobs = {
+        "${name}" = {
+          runs-on = "ubuntu-latest";
+          steps = [
+            { uses = "actions/checkout@v4"; }
+            { uses = "cachix/install-nix-action@v27";
+              "with" = {nix_path = "nixpkgs=channel:nixos-24.05";}; }
+            { run = "nix-build ./mypkgs.nix -A ${name}"; }
+      ];};};
+    }
+  ) mypkgs;
+in pkgs.runCommand "workflows" {} ''
+  mkdir -p $out
+  for yml in ${toString ymls}; do
+    ln -s $yml $out/''${yml#*-}
+  done
+''
